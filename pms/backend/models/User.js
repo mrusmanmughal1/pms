1377755533c8093f092a16804b3cbc1372b87346
Muhammad Usman <mrusmanmughal1gmail.com>
2026-06-15 15:14:28 +0300

    new code

--- pms/backend/models/User.js
+++ pms/backend/models/User.js
@@ -45,6 +45,7 @@ UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+//ok
 
 // Match user entered password to hashed password in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
--- pms/backend/server.js
+++ pms/backend/server.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // Routes
