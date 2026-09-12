86e1f68175866ef3ed1c865bd05e7e0e8d2ee62d
Muhammad Usman <mrusmanmughal1gmail.com>
2026-09-12 15:29:54 +0300

    add comment

--- pms/backend/server.js
+++ pms/backend/server.js
@@ -11,7 +11,7 @@ const uploadRoutes = require("./routes/upload");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// Middleware woking
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
